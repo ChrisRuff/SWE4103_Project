c12696098733df75cb8329b4506b3c8ff1083db7
Chris Ruff <Chrisruff@live.ca>
2020-09-24 22:40:39 -0300

    Added C# testing with CI integration

--- Program.cs
+++ Program.cs
@@ -11,16 +11,17 @@ namespace backend
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+			public static void Main(string[] args)
+			{
+					CreateHostBuilder(args).Build().Run();
+			}
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+			public static IHostBuilder CreateHostBuilder(string[] args) =>
+					Host.CreateDefaultBuilder(args)
+							.ConfigureWebHostDefaults(webBuilder =>
+							{
+									webBuilder.UseStartup<Startup>();
+							});
+
     }
 }
