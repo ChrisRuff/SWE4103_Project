e12f196bd2a80a5823d7302041e97e1a7da0873c
Chris Ruff <Chrisruff@live.ca>
2020-10-05 13:57:19 -0300

    Fixed javascript test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -4,8 +4,17 @@ import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
 
 test('renders learn react link', () => {
-  const { getByText } = render(<Router> <App /> </Router>);
-  const linkElement = getByText(/notindoc/i);
+  const { getByText } = render(getApp());
+  const linkElement = getByText(/Login/i);
   expect(linkElement).toBeInTheDocument();
-
 });
+
+function getApp()
+{
+	return (
+		<div> 
+			<Router> 
+				<App /> 
+			</Router> 
+		</div>);
+}
