8bf173d6050c08e5ad1392ca6315f990ea6e0821
Chris Ruff <Chrisruff@live.ca>
2020-10-11 17:30:49 -0300

    Added class functionality to the database.
    Methods added:
    MakeClass - Makes a class in the db
    RemoveClass - Deletes a class
    ReserveSeat - Sets a seat to be reserved
    DisableSeat - Sets a seat to be disabled
    Also added a method to extract student info from db (GetStudent)

--- backend/Controllers/DatabaseConnector.cs
+++ backend/Controllers/DatabaseConnector.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using backend.Controllers.Models;
 
 using MongoDB.Driver;
 using MongoDB.Bson;
@@ -18,6 +19,7 @@ namespace backend
 		private MongoClient client;
 		private IMongoDatabase database;
 		private IMongoCollection<BsonDocument> students;
+		private IMongoCollection<BsonDocument> classes;
 		private IMongoCollection<BsonDocument> profs;
 
 		private DatabaseConnector()
@@ -29,6 +31,7 @@ namespace backend
 			database = client.GetDatabase("attendance");
 			students = database.GetCollection<BsonDocument>("students");
 			profs = database.GetCollection<BsonDocument>("profs");
+			classes = database.GetCollection<BsonDocument>("classes");
 		}
 
 		public bool CheckPass(string name, string pass)
@@ -51,8 +54,8 @@ namespace backend
 			BsonDocument newStudent = new BsonDocument
 			{
 				{ "name", name },
-				{ "email", email }
-				//{ "pass", pass }
+				{ "email", email },
+				{ "pass", pass }
 			};
 
 			// Insert it into the database
@@ -174,11 +177,144 @@ namespace backend
 				return false;
 			}
 		}
+		public StudentDTO GetStudent(string email)
+		{
+			// Create a filter that finds the student
+			FilterDefinition<BsonDocument> query = 
+				Builders<BsonDocument>.Filter.Eq("email", email);
+
+			var studentsFound = students.Find(query);
+			if(studentsFound.CountDocuments() <= 0)
+			{
+				return null;
+			}
+			var student = studentsFound.First();
+			StudentDTO data = new StudentDTO();
+			data.studentName = student["name"].ToString();
+			data.email = student["email"].ToString();
+			data.pass = student["pass"].ToString();
+			ClassDTO[] classes = new ClassDTO[student["classes"].AsBsonArray.Count];
+			int idx = 0;
+			foreach(var i in student["classes"].AsBsonArray)
+			{
+				var seat = new SeatDTO();
+				seat.x = i[1]["x"].ToInt32();
+				seat.y = i[1]["y"].ToInt32();
+				classes[idx].className = i[0].ToString();
+				classes[idx].seat = seat;
+				idx++;
+			}
+			return data;
+		}
 
 		public void Wipe()
 		{
 			client.DropDatabase("attendance");
 			students = database.GetCollection<BsonDocument>("students");
+			classes = database.GetCollection<BsonDocument>("classes");
+		}
+
+		public bool MakeClass(string name, int width, int height)
+		{
+			// Create a filter that will find the student with the given email
+			FilterDefinition<BsonDocument> query 
+				= Builders<BsonDocument>.Filter.Eq("name", name);
+			if(classes.Find(query).CountDocuments() > 0)
+			{
+				return false;
+			}
+
+			// Create the student document
+			BsonDocument newClass = new BsonDocument
+			{
+				{ "name", name },
+				{ "width", width },
+				{ "height", height },
+				{ "dSeats", new BsonArray{}},
+				{ "rSeats", new BsonArray{}},
+			};
+
+			// Insert it into the database
+			classes.InsertOne(newClass);
+			return true;
+		}
+
+		public bool DisableSeat(string className, int x, int y)
+		{
+			// Create a filter that will find the student with the given email
+			FilterDefinition<BsonDocument> query 
+				= Builders<BsonDocument>.Filter.Eq("name", className);
+
+
+			var foundClasses = classes.Find(query);
+			if(foundClasses.CountDocuments() <= 0)
+			{
+				return false;
+			}
+			var foundClass = foundClasses.First();
+			if(foundClass["width"] < x || foundClass["height"] < y)
+			{
+				return false;
+			}
+			foreach(var i in foundClass["dSeats"].AsBsonArray)
+			{
+				if(i["x"] == x && i["y"] == y)
+					return false;
+			}
+			// Create a update routine that will add a class 
+			// (with absents field to the student document)
+			UpdateDefinition<BsonDocument> update = 
+				Builders<BsonDocument>.Update.AddToSet("dSeats", new BsonDocument{{"x", x}, {"y", y}});
+			classes.UpdateOne(foundClass, update);
+
+			return true;
+		}
+		public bool ReserveSeat(string className, int x, int y)
+		{
+			// Create a filter that will find the student with the given email
+			FilterDefinition<BsonDocument> query 
+				= Builders<BsonDocument>.Filter.Eq("name", className);
+
+
+			var foundClasses = classes.Find(query);
+			if(foundClasses.CountDocuments() <= 0)
+			{
+				return false;
+			}
+			var foundClass = foundClasses.First();
+			if(foundClass["width"] < x || foundClass["height"] < y)
+			{
+				return false;
+			}
+			foreach(var i in foundClass["rSeats"].AsBsonArray)
+			{
+				if(i["x"] == x && i["y"] == y)
+					return false;
+			}
+			// Create a update routine that will add a class 
+			// (with absents field to the student document)
+			UpdateDefinition<BsonDocument> update = 
+				Builders<BsonDocument>.Update.AddToSet("rSeats", new BsonDocument{{"x", x}, {"y", y}});
+			classes.UpdateOne(foundClass, update);
+
+			return true;
+		}
+		public bool RemoveClass(string className)
+		{
+			// Create a filter that will find the student with the given email
+			FilterDefinition<BsonDocument> query 
+				= Builders<BsonDocument>.Filter.Eq("name", className);
+			
+			// Actually delete the student if query finds result
+			if(classes.Find(query).CountDocuments() > 0)
+			{
+				classes.DeleteOne(query);
+				return true;
+			}
+			else
+			{
+				return false;
+			}
 		}
 
 		public static DatabaseConnector Connector
