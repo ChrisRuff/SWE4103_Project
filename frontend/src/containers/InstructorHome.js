79cc3e73fae43615aa1b82bd9ad39a64495e2366
Chris Ruff <Chrisruff@live.ca>
2020-11-01 09:51:29 -0400

    Moved the add button

--- frontend/src/containers/InstructorHome.js
+++ frontend/src/containers/InstructorHome.js
@@ -203,18 +203,21 @@ export default function InstructorHome() {
 return (
     <div>
 		<div className="layoutHeader">
-			<DropdownButton 
-				title={StateManager.getSelectedClass()}
-				id="classDropdown"
-				onSelect={handleSelect.bind(this)}>
-				{classList.map((opt, i) => (
-					<MenuItem key={i} eventKey={i}>
-						{opt}
-					</MenuItem>
-				))}
-			</DropdownButton>
-        <Button onClick={newClass} variant="light">Add</Button>
-        <Button onClick={makeClass} variant="light">Submit</Button>
+			<div style={{width: "100%"}}>
+				<DropdownButton 
+					title={StateManager.getSelectedClass()}
+					id="classDropdown"
+					onSelect={handleSelect.bind(this)}>
+					{classList.map((opt, i) => (
+						<MenuItem key={i} eventKey={i}>
+							{opt}
+						</MenuItem>
+					))}
+				</DropdownButton>
+				<div style={{width: "15px", height: "auto", display: "inline-block"}}/>
+				<Button onClick={newClass} variant="light">Add</Button>
+				<Button onClick={makeClass} variant="light" className="pull-right">Submit</Button>
+			</div>
 		</div>
 		<Fragment>{layout}</Fragment>
 		<div className="layoutFooter">
