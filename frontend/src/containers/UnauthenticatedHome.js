b3273bf6a9a5d035a5f3172f3af43e9c5a6dfca2
Chris Ruff <Chrisruff@live.ca>
2020-11-06 19:52:14 -0400

    Added notification indicator to dropdown and can change it's value in db

--- frontend/src/containers/UnauthenticatedHome.js
+++ frontend/src/containers/UnauthenticatedHome.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./UnauthenticatedHome.css";
+import { useHistory } from "react-router-dom";
 
 export default function UnauthenticatedHome() {
+	const history = useHistory();
+	if(localStorage.getItem("user") != null)
+	{
+		history.push("/InstructorHome"); 
+	}
   return (
     <div className="UnauthenticatedHome">
       <div className="lander">
