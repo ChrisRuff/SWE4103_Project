69f25789ec6b8de4c42a57d79f422e60bf44dfaa
Chris Ruff <Chrisruff@live.ca>
2020-12-02 23:01:22 -0400

    Fix date input

--- frontend/src/containers/InstructorHome.js
+++ frontend/src/containers/InstructorHome.js
@@ -449,7 +449,7 @@ export default function InstructorHome() {
 		let obj = 
 			[{
 				"studentNames": absents, 
-				"date": (selectedDate.getDay() + "/" + selectedDate.getMonth() + "/" + selectedDate.getFullYear()), 
+				"date": (selectedDate.getDate() + "/" + selectedDate.getMonth() + "/" + selectedDate.getFullYear()), 
 				"className": title
 			}];
 
